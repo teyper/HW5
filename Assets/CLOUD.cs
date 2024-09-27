f8f50741b7f50e26c6be5e8fd17d5d8815211843
teyper <teyjhanabartonneal@spelman.edu>
2024-09-26 22:28:02 -0400

    EDITS

--- Assets/CLOUD.cs
+++ Assets/CLOUD.cs
@@ -1,29 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.RuleTile.TilingRuleOutput;
 
-public class BOSS : MonoBehaviour
+public class CloudScroller : MonoBehaviour
 {
+    
     public float speed = 0.005f;
     float startPosX;
-    public float scrollLimit = 9f;
+    public float scrollLimit = 9.0f;
 
     void Start()
     {
+       
         startPosX = transform.position.x;
-
     }
 
-    // Update is called once per frame
     void Update()
     {
+       
         transform.Translate(-speed, 0f, 0f);
-
-        if (transform.position.x < startPosX - scrollLimit);
+        if (transform.position.x < startPosX - scrollLimit)
         {
             transform.position = new Vector3(startPosX + scrollLimit, transform.position.y, transform.position.z);
-
         }
     }
 }

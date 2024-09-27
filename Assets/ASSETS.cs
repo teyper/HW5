013139fdeab693ca42a1e0ba71e5c2a2818014d6
teyper <teyjhanabartonneal@spelman.edu>
2024-09-26 22:56:50 -0400

    final

---
+++ Assets/ASSETS.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// bg photo:    https://ca.pinterest.com/pin/567383253026537102/ 
+//bg music:https://www.youtube.com/watch?v=qAGvQDoL5s4 
+//sprite texture:https://www.vecteezy.com/png/12376071-realistic-cumulus-cloud 
+public class ASSETS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
